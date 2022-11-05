b7c3c4597a3a36ef163f066dc7643e3ba4382cba
macmizy <macmizy@gmail.com>
2022-11-05 13:45:07 +0100

    men at work!

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,15 +1,41 @@
 const express = require('express')
 const blogModel = require('../models/blog.model')
+const userModel = require('../models/user.model')
 const passport = require('passport')
 
 const blogRoute = express.Router()
 
-blogRoute.get('/?page',async(req,res)=>{
+blogRoute.get('/',async(req,res)=>{
     try{
-        const page = req.query.page
+        const {page, orderby,search} = req.query
         const limit = 20
         const skip = (page-1) * limit
-        const blog = await blogModel.find({state: 'published'}).limit(limit).skip(skip)
+
+        if(orderby == 'timestamp'){
+            const blog = await blogModel.find({state: 'published'}).limit(limit).skip(skip).sort({createdAt: 1})
+            return res.status(200).send(blog)
+        }
+        if(orderby == 'reading_time'){
+            const blog = await blogModel.find({state: 'published'}).limit(limit).skip(skip).sort({reading_time: 1})
+            return res.status(200).send(blog)
+        }
+        if(orderby == 'read_count'){
+            const blog = await blogModel.find({state: 'published'}).limit(limit).skip(skip).sort({read_count: 1})
+            return res.status(200).send(blog)
+        }
+        const blog = await blogModel.find({state: 'published'}).and({
+            $or: [
+              {
+                title: { $regex: search, $options: "i" },
+              },
+              {
+                author: { $regex: search, $options: "i" },
+              },
+              {
+                tags: { $regex: search, $options: "i" },
+              }
+            ],
+        }).limit(limit).skip(skip)
         return res.status(200).send(blog)
 
     }catch(err){
@@ -18,11 +44,18 @@ blogRoute.get('/?page',async(req,res)=>{
     }
 })
 
-blogRoute.post('/',async(req,res)=>{
+
+// GET SINGLE BLOG BY ID 
+blogRoute.get('/:blogId', async (req,res)=>{
     try{
-        const body = req.body
-        const blog = await blogModel.create(body)
-        return res.status(200).json(blog)
+        const blogId = req.params.blogId
+        const blog = await blogModel.findById({_id: blogId})
+        blog.read_count++
+        await blog.save()
+         const userId = blog.userId
+         const user = await userModel.findById(userId)
+        return res.send({blog: blog, user: user})
+
 
     }catch(err){
         console.log(err)
@@ -30,12 +63,13 @@ blogRoute.post('/',async(req,res)=>{
     }
 })
 
+
 blogRoute.patch('/updateState/:id', async (req,res)=>{
     try{
         const state  = req.body.state
         const Id = req.params.id
         const updateState = await blogModel.findByIdAndUpdate({_id: Id},{state: state},{new: true})
-        return res.send(updateState)  
+        return res.status(200).send(updateState)  
     }catch(err){
         console.log(err)
         return res.send(err)
@@ -46,7 +80,7 @@ blogRoute.delete('/deleteblog/:id', async (req,res)=>{
     try{
         const Id  = req.params.id
         const blog = await blogModel.deleteOne({ _id: Id})
-        return res.json({ status: true, blog })
+        return res.json({ status: true, ...blog })
 
     }catch(err){
         console.log(err)
