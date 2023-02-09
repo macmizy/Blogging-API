e09a09f5539b9da02bdd77d82bd9eb5721a75b90
macmizy <macmizy@gmail.com>
2023-02-09 13:53:45 +0100

    fixed .gitignore

--- example.env.js
+++ example.env.js
@@ -1,5 +1,8 @@
 {
     PORT = 3000,
     DB_URL = mongodb_URL,
-    SECRET_KEY = something-secret
+    SECRET_KEY = something-secret,
+    clientID = 'google oauth2 client id ',
+    clientSecret = 'Google oauth2 secret',
+    callbackURL = 'http://localhost:3000/auth/google/callback'
 }
