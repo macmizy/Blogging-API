a1b071b91c1625af07c7a06cee1b04fe740c5ba2
macmizy <macmizy@gmail.com>
2023-02-10 12:35:20 +0100

    fixed enoent error

--- db.js
+++ db.js
@@ -1,18 +1,18 @@
-const moogoose = require('mongoose');
+const mongoose = require('mongoose');
 const logger = require('./logger/logger')
 require('dotenv').config();
 
 const DB_URL = process.env.DB_URL;
 
-
+mongoose.set('strictQuery', false)
 function connectToMongoDB() {
-    moogoose.connect(DB_URL);
+    mongoose.connect(DB_URL);
 
-    moogoose.connection.on('connected', () => {
+    mongoose.connection.on('connected', () => {
         logger.info('Connected to MongoDB successfully');
     });
 
-    moogoose.connection.on('error', (err) => {
+    mongoose.connection.on('error', (err) => {
         logger.error(err);
     })
 }
